e0ef11a1e5fb369d2fb0c3ce535dfea4e1fcf27b
oxlt <theebrooklynn@gmail.com>
2024-09-10 19:40:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+myCurrentAge = 20 
+myCurrentYear = 2024
+myNewAge = myCurrentAge + (2050 - myCurrentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
